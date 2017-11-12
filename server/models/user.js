2f7b11b404bad7b8cb5b48a0fd6b3388775d058a
Luis Troya <troyaluis56@gmail.com>
2017-11-11 23:56:40 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function() {
     const user = this;
     const access = 'auth';
     const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
-
+    console.log('User', user);
     user.tokens.push({ access, token });
 
     return user.save().then(() => {
@@ -72,7 +72,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.statics.findByCredentials = function(email, password) {
+    return User.findOne({ email }).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (! res) {
+                    reject();
+                }
+
+                resolve(user);
+            })
+        });
+    });
+};
+
+UserSchema.pre('save', function(next) {
     const user = this;
 
     if (user.isModified('password')) {
